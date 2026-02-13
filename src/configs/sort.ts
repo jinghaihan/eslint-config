30df26fcafac1a22e9a155dd9fc7fe41a2eba2c2
jinghaihan <jhh19980114@gmail.com>
2026-02-13 15:34:51 +0800

    feat: deps sort by catalog name

--- src/configs/sort.ts
+++ src/configs/sort.ts
@@ -1,5 +1,106 @@
 import type { TypedFlatConfigItem } from '@antfu/eslint-config'
+import catalogsSort, { configs } from 'eslint-plugin-catalogs-sort'
 
 export async function sortPackageJson(): Promise<TypedFlatConfigItem[]> {
-  return []
+  return [
+    {
+      name: 'octohash/sort/setup',
+      plugins: {
+        'catalogs-sort': catalogsSort,
+      },
+    },
+    {
+      name: 'octohash/sort/package-json',
+      files: ['**/package.json'],
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            order: [
+              'publisher',
+              'name',
+              'displayName',
+              'type',
+              'version',
+              'private',
+              'packageManager',
+              'description',
+              'author',
+              'contributors',
+              'license',
+              'funding',
+              'homepage',
+              'repository',
+              'bugs',
+              'keywords',
+              'categories',
+              'sideEffects',
+              'imports',
+              'exports',
+              'main',
+              'module',
+              'unpkg',
+              'jsdelivr',
+              'types',
+              'typesVersions',
+              'bin',
+              'icon',
+              'files',
+              'engines',
+              'activationEvents',
+              'contributes',
+              'scripts',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'dependencies',
+              'optionalDependencies',
+              'devDependencies',
+              'pnpm',
+              'overrides',
+              'resolutions',
+              'husky',
+              'simple-git-hooks',
+              'lint-staged',
+              'eslintConfig',
+            ],
+            pathPattern: '^$',
+          },
+          {
+            order: { type: 'asc' },
+            pathPattern: '^workspaces\\.catalog$',
+          },
+          {
+            order: { type: 'asc' },
+            pathPattern: '^workspaces\\.catalogs\\.[^.]+$',
+          },
+          {
+            order: [
+              'types',
+              'import',
+              'require',
+              'default',
+            ],
+            pathPattern: '^exports.*$',
+          },
+          {
+            order: [
+            // client hooks only
+              'pre-commit',
+              'prepare-commit-msg',
+              'commit-msg',
+              'post-commit',
+              'pre-rebase',
+              'post-rewrite',
+              'post-checkout',
+              'post-merge',
+              'pre-push',
+              'pre-auto-gc',
+            ],
+            pathPattern: '^(?:gitHooks|husky|simple-git-hooks)$',
+          },
+        ],
+      },
+    },
+    ...configs.recommended,
+  ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import type { Options, UserConfig } from './types'
 import antfu from '@antfu/eslint-config'
+import { octohash, sortPackageJson, tailwindcss } from './configs'
 
 export function defineConfig(options?: Options, ...userConfigs: UserConfig) {
-  return antfu(
+  const { formatters } = options || {}
+
+  const config = antfu(
     mergeOptions(options),
     ...userConfigs,
   )
+
+  if (formatters === false)
+    return config
+
+  const enableTailwindCSS = typeof options?.formatters === 'boolean' || options?.formatters?.tailwindcss
+  if (enableTailwindCSS) {
+    config
+      .append(tailwindcss(typeof enableTailwindCSS === 'boolean' ? {} : enableTailwindCSS))
+      .renamePlugins({
+        'better-tailwindcss': 'tailwindcss',
+      })
+  }
+
+  config.append(sortPackageJson())
+  config.append(octohash())
+
+  return config
 }
 
 function mergeOptions(options?: Options) {
